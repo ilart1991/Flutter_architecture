595747692880bdf8f773a4d821f2080753f8ccd1
Артем Ильинский <42186657+ilart1991@users.noreply.github.com>
2022-12-09 12:20:10 +0300

    ok

--- twelfth_lesson/lib/internal/application.dart
+++ twelfth_lesson/lib/internal/application.dart
@@ -3,8 +3,14 @@ import 'package:twelfth_lesson/domain/b_logics.dart';
 import 'package:twelfth_lesson/domain/user_model.dart';
 import '/presentation/home.dart';
 
-class Application extends StatelessWidget {
+class Application extends StatefulWidget {
+  @override
+  State<Application> createState() => _ApplicationState();
+}
+
+class _ApplicationState extends State<Application> {
   BLogicsClass varForFunc = BLogicsClass();
+
   User user = User(firstName: "Artem", lastName: "Ilinskii", age: 31);
 
   @override
